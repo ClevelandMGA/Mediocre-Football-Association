923e654c55b764e3fab115e0e2e462a77a5ef4e7
ClevelandMGA <122036716+ClevelandMGA@users.noreply.github.com>
2023-01-05 11:41:32 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -119,7 +119,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Cleveland, OH", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/jose.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/ben.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2022, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "cle", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
